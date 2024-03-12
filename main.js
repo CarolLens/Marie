29dde0459316f2c3219e4bff0814a650aa5ddaa1
CarolLens <163136359+CarolLens@users.noreply.github.com>
2024-03-12 09:39:47 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+function mudarimagemfora()
+{
+    document.getElementById("imagem").src ="https://lizaeventos.loja2.com.br/img/0498827fca96f827e88457a75b387e5c.jpg";
+}
+
+function mudarimagemdentro()
+{
+    document.getElementById("imagem").src ="https://i.pinimg.com/originals/bf/e3/27/bfe327110078406102ffae49bcb122f0.jpg"
+}
